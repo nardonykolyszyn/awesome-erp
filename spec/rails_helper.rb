e7fbb578865e11382e419672949d52ba595d6d7d
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-17 04:03:45 -0500

    avoid record not found on show

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,11 +43,6 @@ ActiveRecord::Migration.maintain_test_schema!
 	  config.integrate do |with|
 	    # Choose a test framework:
 	    with.test_framework :rspec
-
-	    # Choose one or more libraries:
-	    with.library :active_record
-	    with.library :active_model
-	    # Or, choose the following (which implies all of the above):
 	    with.library :rails
 	  end
 	end
