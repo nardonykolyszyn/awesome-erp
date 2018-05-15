99acb7f269094a855f03f8b88b265df989475908
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-05-15 02:09:28 -0500

    starting migration from trestle

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  resources :sales do
-    member do
-      post 'close'
-    end
-  end
-  root 'home#index'
-  get 'home/index'
-
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  resources :articles
-  resources :categories, only: [:index]
-  resources :enterprises, only: [:new, :create, :update, :edit]
-  resources :tasks
-  resources :projects
+  root to: "trestle/dashboard#index"
 end
